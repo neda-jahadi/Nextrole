e745444d43cb667137f441886b2e12943e8b6ad5
neda-jahadi <55974643+neda-jahadi@users.noreply.github.com>
2026-09-16 17:05:18 +0200

    feat: use skeleton for add job form loading

--- client/src/pages/job/AddJobPage.tsx
+++ client/src/pages/job/AddJobPage.tsx
@@ -1,6 +1,6 @@
 import { useMunicipalities } from '@/features/locations/api/locationQuery';
 import JobForm from '@/features/jobs/forms/JobForm';
-import Spinner from '@/components/ui/Spinner';
+import JobFormSkeleton from '@/features/jobs/components/JobFormSkeleton';
 import NotFound from '@/pages/NotFound';
 import Section from '@/components/layouts/Section';
 import Container from '@/components/layouts/Container';
@@ -9,7 +9,7 @@ import Panel from '@/components/layouts/Panel';
 const AddJobPage = () => {
   const { data: municipalities, isLoading, isError } = useMunicipalities();
 
-  if (isLoading) return <Spinner loading={true} />;
+  if (isLoading) return <JobFormSkeleton />;
   if (isError || !municipalities) return <NotFound />;
 
   return (
