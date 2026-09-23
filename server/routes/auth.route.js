03d1514ec7f9f67535dcdc4de30c7ffdd275850f
neda-jahadi <neda_jahadi@yahoo.com>
2026-09-23 10:52:32 +0200

    Add rate limiting to password login

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -10,11 +10,12 @@ import {
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 import { loginSchema, registerSchema } from '../validators/authValidators.js';
 import { validateRequest } from '../middlewares/validateRequest.js';
+import { authLimiter } from '../middlewares/rateLimitMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', validateRequest(registerSchema), registerUser);
-router.post('/login', validateRequest(loginSchema), loginUser);
+router.post('/login', authLimiter, validateRequest(loginSchema), loginUser);
 router.post('/logout', logoutUser);
 router.get('/me', authMiddleware, getMe);
 router.get('/google', startGoogleOAuth);
